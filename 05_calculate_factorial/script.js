1e129e41ef0871d55aa544abda54f036a8dc18c3
Roshan Patil <patilroshan9687@gmail.com>
2025-07-29 14:36:33 +0530

    applying reduce method

--- 05_calculate_factorial/script.js
+++ 05_calculate_factorial/script.js
@@ -1,13 +1,53 @@
+// using loop 
+
 const btnCalculate = document.querySelector("#calculateBtn")
 const resetBtn = document.querySelector("#resetBtn")
 const result = document.querySelector("#result")
 
 
+// btnCalculate.addEventListener("click", calculateFactorial);
+
+// function calculateFactorial() {
+//     let number = document.querySelector("#numberInput").value.trim();
+//     let factorialNumber = 1;
+//     result.textContent = "";
+//     if (number === "") {
+//         result.textContent = "Please enter a number";
+//         return;
+//     }
+//     number = parseInt(number);
+//     if (isNaN(number)) {
+//         result.textContent = "Please enter a valid number";
+//     } else if (number < 0) {
+//         result.textContent = "Please enter a positive number";
+//     } else if (number === 0) {
+//         displayresult(1);
+//     } else {
+//         for (let i = 1; i <= number; i++) {
+//             factorialNumber *= i;
+//         }
+//         displayresult(factorialNumber);
+//     }
+// }
+
+// function displayresult(value) {
+//     result.textContent = `${value}`;
+// }
+
+// resetBtn.addEventListener('click', resetForm);
+
+// function resetForm() {
+//     document.querySelector('#numberInput').value = ''; // clear input
+//     result.textContent = ''; // clear result
+// }
+
+// using reduce
+
 btnCalculate.addEventListener("click", calculateFactorial);
 
 function calculateFactorial() {
     let number = document.querySelector("#numberInput").value.trim();
-    let factorialNumber = 1;
+     let factorialNumber = 1;   
     result.textContent = "";
     if (number === "") {
         result.textContent = "Please enter a number";
@@ -18,12 +58,11 @@ function calculateFactorial() {
         result.textContent = "Please enter a valid number";
     } else if (number < 0) {
         result.textContent = "Please enter a positive number";
-    } else if (number === 0) {
+    } else if (number === 0 || number === 1) {
         displayresult(1);
     } else {
-        for (let i = 1; i <= number; i++) {
-            factorialNumber *= i;
-        }
+         factorialNumber = Array.from({ length: number }, (_, i) => i + 1)
+            .reduce((acc, curr) => acc * curr, 1);
         displayresult(factorialNumber);
     }
 }
