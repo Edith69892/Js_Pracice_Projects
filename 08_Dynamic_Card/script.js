be5ef234a165fd05d39e37bf48c63cd0534176a7
Roshan Patil <patilroshan9687@gmail.com>
2025-07-31 16:32:41 +0530

    :replaced innerHTML with appendChild to allow adding multiple cards

--- 08_Dynamic_Card/script.js
+++ 08_Dynamic_Card/script.js
@@ -2,40 +2,70 @@ const Cards = document.getElementById('cardsArea');
 const btn = document.querySelector('#addBtn');
 
 btn.addEventListener('click', () => {
-   const newCard = createCard(
+   let newCard = createCard(
         'Dynamic Card Title', 'Edith',
         1234567, '31:06', 2,'https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw')
-    Cards.innerHTML += newCard;
+   Cards.appendChild(newCard);
     
 })
 
-function createCard(title, authorName, views, duration, monthOld,thumbnail) {
-let viewStr ;
-if (views >= 1000000) {
-    viewStr = (views / 1000000).toFixed(1) + 'M';
-} else if (views >= 1000) {
-    viewStr = (views / 1000).toFixed(1) + 'K';
-}
-else {
-    viewStr = views.toString();
-}
+// function createCard(title, authorName, views, duration, monthOld,thumbnail) {
+// let viewStr ;
+// if (views >= 1000000) {
+//     viewStr = (views / 1000000).toFixed(1) + 'M';
+// } else if (views >= 1000) {
+//     viewStr = (views / 1000).toFixed(1) + 'K';
+// }
+// else {
+//     viewStr = views.toString();
+// }
 
-console.log(typeof viewStr);
+// // console.log(typeof viewStr);
 
-    let html =` <div class="card">
-         <div class="image">
-                <img src="${thumbnail}"
-                    alt="">
-                <div class="capsule">${duration}</div>
+//     let html =` <div class="card">
+//          <div class="image">
+//                 <img src="${thumbnail}"
+//                     alt="">
+//                 <div class="capsule">${duration}</div>
+//             </div>
+//             <div class="text-content">
+//                 <h2 class="title">${title}</h2>
+//                 <div class="text">
+//                     <span id="author">${authorName}</span>
+//                     <p><span id="views">${viewStr}</span>Views</p>
+//                     <p><span id="publish"> ${monthOld}</span>Months ago</p>
+//                 </div>
+//       </div>`;
+
+//    return html;
+// }
+
+function createCard(title, authorName, views, duration, monthOld, thumbnail) {
+    let viewStr;
+    if (views >= 1000000) {
+        viewStr = (views / 1000000).toFixed(1) + 'M';
+    } else if (views >= 1000) {
+        viewStr = (views / 1000).toFixed(1) + 'K';
+    } else {
+        viewStr = views.toString();
+    }
+
+    // Create card DOM structure
+    let card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+        <div class="image">
+            <img src="${thumbnail}" alt="">
+            <div class="capsule">${duration}</div>
+        </div>
+        <div class="text-content">
+            <h2 class="title">${title}</h2>
+            <div class="text">
+                <span id="author">${authorName}</span>
+                <p><span id="views">${viewStr}</span> Views</p>
+                <p><span id="publish">${monthOld}</span> Months ago</p>
             </div>
-            <div class="text-content">
-                <h2 class="title">${title}</h2>
-                <div class="text">
-                    <span id="author">${authorName}</span>
-                    <p><span id="views">${viewStr}</span>Views</p>
-                    <p><span id="publish"> ${monthOld}</span>Months ago</p>
-                </div>
-      </div>`;
-
-   return html;
+        </div>
+    `;
+    return card;
 }
